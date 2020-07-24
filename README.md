078151e2c405643899d165d1097838321d6ea2c5
James Ward <james@jamesward.com>
2020-07-24 10:23:38 -0600

    init

--- README.md
+++ README.md
@@ -3,17 +3,17 @@ Sample Java Maven
 
 Run Locally (with Java 11 installed):
 ```
-./mvnw compile exec:java
+./mvnw spring-boot:run
 ```
 
 Build Docker Container:
 ```
-./mvnw compile jib:dockerBuild -Dimage=comparing-docker-methods:jib
+./mvnw compile jib:dockerBuild -Dimage=comparing-docker-methods:jib-springboot
 ```
 
 Run Locally with Docker:
 ```
-docker run -it -ePORT=8080 -p8080:8080 comparing-docker-methods:jib
+docker run -it -ePORT=8080 -p8080:8080 comparing-docker-methods:jib-springboot
 ```
 
 Run on Cloud Run (with two clicks):
--- src/main/java/com/google/WebApp.java
+++ src/main/java/com/google/WebApp.java
@@ -1,26 +1,21 @@
 package com.google;
 
-import com.sun.net.httpserver.HttpServer;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@RestController
 public class WebApp {
 
-    public static void main(String[] args) throws IOException {
-        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
-        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-
-        server.createContext("/", handler -> {
-            byte[] response = "hello, world".getBytes();
-            handler.sendResponseHeaders(200, response.length);
-            try (OutputStream os = handler.getResponseBody()) {
-                os.write(response);
-            }
-        });
-
-        System.out.println("Listening at http://localhost:" + port);
-
-        server.start();
+    public static void main(String[] args) {
+        SpringApplication.run(WebApp.class, args);
     }
+
+    @RequestMapping("/")
+    public String index() {
+        return "hello, world";
+    }
+
 }
